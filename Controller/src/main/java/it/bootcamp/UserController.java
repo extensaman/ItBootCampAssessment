9ab255fbf8fed01fd76a36e3e9d0048a4b371fe8
extensaman <verus.wedding@gmail.com>
2023-03-26 10:04:44 +0300

    feat: add ExceptionHandler for Validation

--- Controller/src/main/java/it/bootcamp/UserController.java
+++ Controller/src/main/java/it/bootcamp/UserController.java
@@ -7,14 +7,20 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -59,10 +65,22 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<UserDto> addUser(@RequestBody @Valid UserDto userDto) {
+    public ResponseEntity<UserDto> addUser(@Valid @RequestBody UserDto userDto) {
         logger.info("METHOD::addUser");
         UserDto userDtoFromRepo = userService.addUser(userDto);
         return new ResponseEntity<>(userDtoFromRepo, HttpStatus.CREATED);
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException e) {
+        logger.warn("METHOD::handleValidationException");
+        Map<String, String> response = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            response.put(fieldName, errorMessage);
+        });
+        return response;
+    }
 }
